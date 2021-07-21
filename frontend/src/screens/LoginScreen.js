a01518d033fa51085aec1cfbbfb52ed97c371e4b
lorenzozaccagnini <lorenzozcg@gmail.com>
2021-07-21 12:54:00 +0200

    users

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -62,7 +62,7 @@ const LoginScreen = ({ location, history }) => {
 
       <Row className='py-3'>
         <Col>
-          New Customer?{' '}
+          New user?{' '}
           <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
             Register
           </Link>
